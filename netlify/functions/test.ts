cc85c1a99c7609fac50213cef7bad2d9c8eeb2d4
Andrew Dolan <andrew@dolan.co.nz>
2021-07-04 17:30:26 +1200

    Updated test serverless function

--- netlify/functions/test.ts
+++ netlify/functions/test.ts
@@ -1,6 +1,27 @@
 import { Handler } from "@netlify/functions";
+import { request, gql } from "graphql-request";
 
 const handler: Handler = async (event, context) => {
+	const query = gql`
+		{
+			launchesPast(limit: 10) {
+				mission_name
+				launch_date_local
+				launch_site {
+					site_name_long
+				}
+			}
+		}
+	`;
+
+	request("https://api.spacex.land/graphql/", query).then((data: any) => {
+		console.log(data)
+		return {
+			statusCode: 200,
+			body: JSON.stringify(data)
+		}
+	});
+
 	return {
 		statusCode: 200,
 		body: JSON.stringify({ message: "Hello World" }),
