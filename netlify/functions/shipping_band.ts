c44a94ba88631d88724b5cdf6a69842991a60a91
Andrew Dolan <andrew@dolan.co.nz>
2021-07-25 22:12:08 +1200

    Updated serverless function to support CORS

--- netlify/functions/shipping_band.ts
+++ netlify/functions/shipping_band.ts
@@ -50,6 +50,9 @@ const handler: Handler = async (event, context) => {
 		statusCode: 200,
 		headers: {
 			"Content-Type": "application/json",
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Headers': 'Content-Type',
+			'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
 		},
 		body: JSON.stringify(requestData),
 	};
